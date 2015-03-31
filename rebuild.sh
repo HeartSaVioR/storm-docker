46b1c362d19072403d8e8b1d1b57942fbe66cce2
Jungtaek Lim <kabhwan@gmail.com>
2015-04-01 00:26:08 +0900

    WIP - Test on STORM-615-V2

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm" storm
-docker build -t="wurstmeister/storm-nimbus" storm-nimbus
-docker build -t="wurstmeister/storm-supervisor" storm-supervisor
-docker build -t="wurstmeister/storm-ui" storm-ui
+docker build -t="heartsavior/storm" storm
+docker build -t="heartsavior/storm-nimbus" storm-nimbus
+docker build -t="heartsavior/storm-supervisor" storm-supervisor
+docker build -t="heartsavior/storm-ui" storm-ui
